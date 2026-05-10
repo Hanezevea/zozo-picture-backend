e29fca9366045e85f53ff8554917d9be958fc17a
zhuozhuo <755265041@qq.com>
2026-05-10 12:53:45 +0800

    配置跨域，测试

--- src/main/java/com/zozo/zozopicturebackend/common/BaseResponse.java
+++ src/main/java/com/zozo/zozopicturebackend/common/BaseResponse.java
@@ -13,14 +13,21 @@ public class BaseResponse<T> implements Serializable {
 
     private int code;
 
+    private T data;
+
     private String message;
 
-    private T data;
+
 
     public BaseResponse(int code, T data, String message) {
         this.code = code;
-        this.message = message;
         this.data = data;
+        this.message = message;
+
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "");
     }
 
 
