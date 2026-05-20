2e30cc87f1ec963318180dc7c7c2964a019d1371
zhuozhuo <755265041@qq.com>
2026-05-20 21:29:48 +0800

    以图搜图功能，需要后续修改

--- src/main/java/com/zozo/zozopicturebackend/controller/PictureController.java
+++ src/main/java/com/zozo/zozopicturebackend/controller/PictureController.java
@@ -7,6 +7,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.zozo.zozopicturebackend.annotaion.AuthCheck;
+import com.zozo.zozopicturebackend.api.imagesearch.ImageSearchApiFacade;
+import com.zozo.zozopicturebackend.api.imagesearch.model.ImageSearchResult;
 import com.zozo.zozopicturebackend.common.BaseResponse;
 import com.zozo.zozopicturebackend.common.DeleteRequest;
 import com.zozo.zozopicturebackend.common.ResultUtils;
@@ -328,6 +330,22 @@ public class PictureController {
         int uploadCount =  pictureService.uploadPictureByBatch(pictureUploadByBatchRequest, loginUser);
         return ResultUtils.success(uploadCount);
     }
+
+
+    /**
+     * 以图搜图
+     */
+    @PostMapping("/search/picture")
+    public BaseResponse<List<ImageSearchResult>> searchPictureByPicture(@RequestBody SearchPictureByPictureRequest searchPictureByPictureRequest) {
+        ThrowUtils.throwIf(searchPictureByPictureRequest == null, ErrorCode.PARAMS_ERROR);
+        Long pictureId = searchPictureByPictureRequest.getPictureId();
+        ThrowUtils.throwIf(pictureId == null || pictureId <= 0, ErrorCode.PARAMS_ERROR);
+        Picture oldPicture = pictureService.getById(pictureId);
+        ThrowUtils.throwIf(oldPicture == null, ErrorCode.NOT_FOUND_ERROR);
+        List<ImageSearchResult> resultList = ImageSearchApiFacade.searchImage(oldPicture.getUrl());
+        return ResultUtils.success(resultList);
+    }
+
 
 
 
